b520b452e31add56058bf2037ac56ddd5a2ff106
swimmingbubble <jaocfb@gmail.com>
2022-04-26 22:45:35 +0100

    u
    kkk

--- jj.R
+++ jj.R
@@ -1,1 +1,10 @@
 ola jj 
+dat<-as.data.frame(admissions20)
+plot(admissions20[12], ylim=c(0,500))
+x<-admissions20[12]
+meanf<-function(t){
+  
+  temp_b<-10*log(1+t/50)+12*(1-exp(-30*t))*(1+sin(2*pi*0.00277778*t))+2*pi*12*0.00277778*t
+  return(temp_b)
+}
+curve(meanf,0,3000, xlab = "t", ylab = "m(t)")
